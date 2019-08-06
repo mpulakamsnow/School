1444ffdd0ad0e8c2589aa428067dda9adada6a08
mpulakam <mpulakam@USMJ0261P5.CSCMWS.CSCMWS.COM>
2019-08-06 05:27:07 -0500

    School Updates
    fromMahendra

---
+++ com.school/src/main/java/HelloWorld/com/school/App.java
@@ -0,0 +1,13 @@
+package HelloWorld.com.school;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
